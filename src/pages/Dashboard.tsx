c46cad2fabd50139a5dacede0cc5afbe6b5f1644
fruxc <hammadansari17@gmail.com>
2020-02-11 15:51:42 +0530

    MCA DEPT

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,50 @@
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from '@ionic/react';
+import { book, build, colorFill, grid } from 'ionicons/icons';
+import React from 'react';
+import './Dashboard.css';
+
+const Dashboard: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>MCA DEPT</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      
+      <img src="/assets/Entrance.jpg" alt="" />
+      <IonContent>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle>About Us</IonCardSubtitle>
+            <IonCardTitle>Welcome to SPIT</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <p>
+              Now that your app has been created, you'll want to start building out features and
+              components. Check out some of the resources below for next steps.
+            </p>
+          </IonCardContent>
+        </IonCard>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Dashboard;
