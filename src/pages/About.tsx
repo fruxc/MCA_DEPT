0e02a1af1231519a80d61a3f3d933a75e4280ad0
fruxc <hammadansari17@gmail.com>
2020-02-18 11:00:52 +0530

    Tab bar

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,13 +3,10 @@ import {
   IonHeader,
   IonPage,
   IonTitle,
-  IonToolbar
+  IonToolbar,
+  IonItem
 } from "@ionic/react";
-import {
-  createMuiTheme,
-  ThemeProvider,
-  responsiveFontSizes
-} from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import ExpansionPanel from "@material-ui/core/ExpansionPanel";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
@@ -18,16 +15,14 @@ import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import React from "react";
 import "./Dashboard.css";
 
-let theme = createMuiTheme({
-  typography: {
-    h5: {
-      alignContent: "center"
-    }
+const useStyles = makeStyles({
+  root: {
+    width: "100%"
   }
 });
-theme = responsiveFontSizes(theme);
 
 const About: React.FC = () => {
+  const classes = useStyles();
   return (
     <IonPage>
       <IonHeader>
@@ -37,63 +32,141 @@ const About: React.FC = () => {
       </IonHeader>
       <img src="/assets/Entrance.jpg" alt="" />
       <IonContent>
-        <Typography variant="h5">Welcome to SPIT</Typography>
-        <ExpansionPanel square={false}>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
+        <div className={classes.root}>
+          <Typography
+            color="textPrimary"
+            variant="h5"
+            style={{ textAlign: "center" }}
           >
-            <Typography>About Us</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography variant="body2">
-              The institute is located in 47 acres of green campus at Andheri
-              (W), the fastest growing suburb of Mumbai. The campus also houses
-              four Bhavan’s Institutions of great repute namely Bhavan’s College
-              (the arts, commerce and science college), Sardar Patel College of
-              Engineering – Government aided Engineering college, S.P. Jain
-              Institute of Management and Research, a management institute and
-              A.H. Wadia, higher secondary school.
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel><ExpansionPanel square={false}>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography>Institutional Value</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography variant="body2">
-              1. Integrity.<br></br>
-              2. Excellence <br></br>
-              3. Social Sensitivity <br></br>
-              4. Globalization<br></br>
-              <hr></hr>
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel><ExpansionPanel square={false}>
-          <ExpansionPanelSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography>About Us</Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography variant="body2">
-              The institute is located in 47 acres of green campus at Andheri
-              (W), the fastest growing suburb of Mumbai. The campus also houses
-              four Bhavan’s Institutions of great repute namely Bhavan’s College
-              (the arts, commerce and science college), Sardar Patel College of
-              Engineering – Government aided Engineering college, S.P. Jain
-              Institute of Management and Research, a management institute and
-              A.H. Wadia, higher secondary school.
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
+            Welcome to SPIT
+          </Typography>
+          <ExpansionPanel square={false}>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>About Us</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography variant="body2">
+                The institute is located in 47 acres of green campus at Andheri
+                (W), the fastest growing suburb of Mumbai. The campus also
+                houses four Bhavan’s Institutions of great repute namely
+                Bhavan’s College (the arts, commerce and science college),
+                Sardar Patel College of Engineering – Government aided
+                Engineering college, S.P. Jain Institute of Management and
+                Research, a management institute and A.H. Wadia, higher
+                secondary school.
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+          <ExpansionPanel square={false}>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Institutional Value</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography variant="body2">
+                1. Integrity.<br></br>
+                2. Excellence <br></br>
+                3. Social Sensitivity <br></br>
+                4. Globalization<br></br>
+                <hr></hr>
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+          <ExpansionPanel square={false}>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Vision &amp; Mission</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography variant="body2">
+                <b>Our vision</b>
+                <br></br>
+                <br></br>
+                <IonItem>
+                  Keeping in view the growing and changing needs of Industry and
+                  society, we at S.P.I.T. are committed to creating an
+                  environment which will raise the intellectual and moral
+                  standards of our students. Our endeavor is to strive for the
+                  overall development of students, thereby enabling them to
+                  accept challenges. In tune with this our vision is<br></br>
+                  <br></br>
+                </IonItem>
+                <IonItem>
+                  <b>
+                    To build a renowned institute which will produce graduate
+                    engineers with global competency and social sensitivity.
+                  </b>
+                </IonItem>
+                <br></br>
+                <br></br>
+                <b>Our Mission</b>
+                <br></br>
+                <br></br>
+                <IonItem> 
+                  Provide high quality education in engineering and technology
+                  promoting the Indian Values and Ethos that will prepare the
+                  participants to lead lives of personal integrity and civic
+                  responsibility in a global society.<br></br>
+                </IonItem>
+                <IonItem>
+                  Promote an Educational Environment that combines academic
+                  study with the excitement of intellectual curiosity for
+                  engineers of tomorrow.<br></br>
+                </IonItem>
+                <IonItem>
+                  Enhance career opportunities for students through
+                  Industry-Institute interaction, value-added courses and
+                  projects in cutting edge technology.<br></br>
+                </IonItem>
+                <IonItem>
+                  Inculcate Entrepreneurial mindset in students to make them job
+                  creators.<br></br>
+                </IonItem>
+                <IonItem>
+                  Focus on applied research to create next generation
+                  technologies.<br></br>
+                </IonItem>
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+          <ExpansionPanel square={false}>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Founders Message</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography variant="body2">
+                “Education would fail ignominiously in its objective if it
+                manufactures only a robot and called him an economic man
+                stressing the adjective economic and forgetting the substantive
+                man. A university cannot afford to ignore the cultural aspects
+                of education whatever studies it specilizes in. Science is a
+                means, not an end. Whereas culture is an end in itself. Even
+                though you may ultimately become a scientist, a doctor, or an
+                engineer, you must while in college, absorb fundamental values
+                which will make you a man of culture. An engineer has not merely
+                to build bridges; he has to be a devoted husband, a kind father,
+                a friendly neighbour, a dutiful citizen, and a man true to
+                himself. He will have trials and tribulations; his heart will
+                fail him at times; he will then need the strength which true
+                culture alone can give.”
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+        </div>
       </IonContent>
     </IonPage>
   );
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,8 +3,18 @@ import {
   IonHeader,
   IonPage,
   IonTitle,
-  IonToolbar
+  IonToolbar,
+  IonTabs,
+  IonLabel,
+  IonTabButton,
+  IonIcon,
+  IonTabBar,
+  IonRouterOutlet,
 } from '@ionic/react';
+import Profile from './Profile';
+import About from './About';
+import Details from './Details';
+import { Redirect, Route } from 'react-router-dom';
 import React from 'react';
 import './Dashboard.css';
 
@@ -17,6 +27,36 @@ const Dashboard: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
+    <IonTabs>
+        <IonRouterOutlet>
+          <Route path="/dashboard" component={Dashboard} exact={true} />
+          <Route path="/profile" component={Profile} exact={true} />
+          <Route path="/profile/details" component={Details} />
+          <Route path="/about" component={About} exact={true}/>
+          <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
+        </IonRouterOutlet>
+      <IonTabBar slot="bottom">
+        <IonTabButton tab="Dashboard">
+          <IonIcon name="calendar" />
+          <IonLabel>Schedule</IonLabel>
+        </IonTabButton>
+
+        <IonTabButton tab="Dashboard/Details">
+          <IonIcon name="contacts" />
+          <IonLabel>Speakers</IonLabel>
+        </IonTabButton>
+
+        <IonTabButton tab="Profile">
+          <IonIcon name="map" />
+          <IonLabel>Map</IonLabel>
+        </IonTabButton>
+
+        <IonTabButton tab="About">
+          <IonIcon name="information-circle" />
+          <IonLabel>About</IonLabel>
+        </IonTabButton>
+      </IonTabBar>
+    </IonTabs>
       </IonContent>
     </IonPage>
   );
